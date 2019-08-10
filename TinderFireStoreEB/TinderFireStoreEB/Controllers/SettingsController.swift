4e2980ed86d16ee638de5e96dacd7168f023c922
Eugene Berezin <berezin.nlp@gmail.com>
2019-08-09 17:46:44 -0700

    save images

--- TinderFireStoreEB/TinderFireStoreEB/Controllers/SettingsController.swift
+++ TinderFireStoreEB/TinderFireStoreEB/Controllers/SettingsController.swift
@@ -12,18 +12,72 @@ import JGProgressHUD
 import SDWebImage
 
 class CustomImagePickerController: UIImagePickerController {
+    
     var imageButton: UIButton?
     
 }
 
-
 class SettingsController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    //instance properties
+    // instance properties
     lazy var image1Button = createButton(selector: #selector(handleSelectPhoto))
     lazy var image2Button = createButton(selector: #selector(handleSelectPhoto))
     lazy var image3Button = createButton(selector: #selector(handleSelectPhoto))
-
+    
+    @objc func handleSelectPhoto(button: UIButton) {
+        print("Select photo with button:", button)
+        let imagePicker = CustomImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.imageButton = button
+        present(imagePicker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let selectedImage = info[.originalImage] as? UIImage
+        // how do i set the image on my buttons when I select a photo?
+        let imageButton = (picker as? CustomImagePickerController)?.imageButton
+        imageButton?.setImage(selectedImage?.withRenderingMode(.alwaysOriginal), for: .normal)
+        dismiss(animated: true)
+        
+        let filename = UUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/images/\(filename)")
+        guard let uploadData = selectedImage?.jpegData(compressionQuality: 0.75) else { return }
+        
+        let hud = JGProgressHUD(style: .dark)
+        hud.textLabel.text = "Uploading image..."
+        hud.show(in: view)
+        
+        
+        ref.putData(uploadData, metadata: nil) { (nil, err) in
+            if let err = err {
+                hud.dismiss()
+                print(">>>>>Failed to upload image to storage:", err)
+                return
+            }
+            
+            print(">>>>>>Finished uploading image")
+            ref.downloadURL(completion: { (url, err) in
+                
+                hud.dismiss()
+                
+                if let err = err {
+                    print(">>>>>Failed to retrieve download URL:", err)
+                    return
+                }
+                
+                print(">>>>>>Finished getting download url:", url?.absoluteString ?? "")
+                
+                if imageButton == self.image1Button {
+                    self.user?.imageUrl1 = url?.absoluteString
+                } else if imageButton == self.image2Button {
+                    self.user?.imageUrl2 = url?.absoluteString
+                } else {
+                    self.user?.imageUrl3 = url?.absoluteString
+                }
+            })
+        }
+    }
+    
     func createButton(selector: Selector) -> UIButton {
         let button = UIButton(type: .system)
         button.setTitle("Select Photo", for: .normal)
@@ -37,37 +91,83 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setupNavigationItems()
-        tableView.backgroundColor = #colorLiteral(red: 0.8894340479, green: 0.8894340479, blue: 0.8894340479, alpha: 1)
+        tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
         tableView.tableFooterView = UIView()
         tableView.keyboardDismissMode = .interactive
-        fetchCurrentUser()
         
+        fetchCurrentUser()
     }
     
+    var user: User?
+    
+    fileprivate func fetchCurrentUser() {
+        // fetch some Firestore Data
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        Firestore.firestore().collection("users").document(uid).getDocument { (snapshot, err) in
+            if let err = err {
+                print(err)
+                return
+            }
+            
+            // fetched our user here
+            guard let dictionary = snapshot?.data() else { return }
+            self.user = User(dictionary: dictionary)
+            self.loadUserPhotos()
+            
+            self.tableView.reloadData()
+            
+            print(">>>>USER", self.user)
+        }
+    }
+    
+    fileprivate func loadUserPhotos() {
+        if let imageUrl = user?.imageUrl1, let url = URL(string: imageUrl) {
+            SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
+                self.image1Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+            }
+        }
+        if let imageUrl = user?.imageUrl2, let url = URL(string: imageUrl) {
+            SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
+                self.image2Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+            }
+        }
+        if let imageUrl = user?.imageUrl3, let url = URL(string: imageUrl) {
+            SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
+                self.image3Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+            }
+        }
+    }
     
     lazy var header: UIView = {
         let header = UIView()
         header.addSubview(image1Button)
         let padding: CGFloat = 16
-        setupSelectPhotoLargeButton(header, padding)
-        setUpStackViewOfRightHandButtons(padding, header)
+        image1Button.anchor(top: header.topAnchor, leading: header.leadingAnchor, bottom: header.bottomAnchor, trailing: nil, padding: .init(top: padding, left: padding, bottom: padding, right: 0))
+        image1Button.widthAnchor.constraint(equalTo: header.widthAnchor, multiplier: 0.45).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [image2Button, image3Button])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = padding
+        
+        header.addSubview(stackView)
+        stackView.anchor(top: header.topAnchor, leading: image1Button.trailingAnchor, bottom: header.bottomAnchor, trailing: header.trailingAnchor, padding: .init(top: padding, left: padding, bottom: padding, right: padding))
         return header
     }()
     
-    class HeaderLable: UILabel {
+    class HeaderLabel: UILabel {
         override func drawText(in rect: CGRect) {
             super.drawText(in: rect.insetBy(dx: 16, dy: 0))
         }
     }
-        
-        
+    
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         if section == 0 {
             return header
         }
-        let headerLabel = HeaderLable()
-        
+        let headerLabel = HeaderLabel()
         switch section {
         case 1:
             headerLabel.text = "Name"
@@ -78,9 +178,6 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         default:
             headerLabel.text = "Bio"
         }
-        
-        headerLabel.isAccessibilityElement = true
-        
         return headerLabel
     }
     
@@ -88,13 +185,12 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         if section == 0 {
             return 300
         }
-         return 40
+        return 40
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 5
     }
-    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return section == 0 ? 0 : 1
@@ -105,134 +201,24 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         
         switch indexPath.section {
         case 1:
-            cell.textField.placeholder = "Name"
+            cell.textField.placeholder = "Enter Name"
             cell.textField.text = user?.name
             cell.textField.addTarget(self, action: #selector(handleNameChange), for: .editingChanged)
         case 2:
-            cell.textField.placeholder = "Profession"
+            cell.textField.placeholder = "Enter Profession"
             cell.textField.text = user?.profession
             cell.textField.addTarget(self, action: #selector(handleProfessionChange), for: .editingChanged)
         case 3:
-            cell.textField.placeholder = "Age"
-            if let age = user?.age {
-              cell.textField.text = String(age)
-            }
+            cell.textField.placeholder = "Enter Age"
             cell.textField.addTarget(self, action: #selector(handleAgeChange), for: .editingChanged)
-            
+            if let age = user?.age {
+                cell.textField.text = String(age)
+            }
         default:
-            cell.textField.placeholder = "Bio"
-            
-        } 
+            cell.textField.placeholder = "Enter Bio"
+        }
         
         return cell
-    }
-    
-    var user: User?
-    
-    fileprivate func fetchCurrentUser() {
-        //fatch current logged in user
-        guard let uid = Auth.auth().currentUser?.uid else {return}
-        Firestore.firestore().collection("users").document(uid).getDocument { (snapshot, err) in
-            if let err = err {
-                print(err)
-            }
-            
-            //fetched user
-            
-            guard let userDictionary = snapshot?.data() else {return}
-            self.user = User(dictionary: userDictionary)
-            self.loadUserPhotos()
-
-
-            self.tableView.reloadData()
-            
-            
-            
-        }
-        
-    }
-    
-    
-    fileprivate func loadUserPhotos() {
-        guard let imageUrl = user?.imageUrl1, let url = URL(string: imageUrl) else { return }
-        // why exactly do we use this SDWebImageManager class to load our images?
-        SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
-            self.image1Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
-        }
-        
-    }
-    
-    fileprivate func setUpStackViewOfRightHandButtons(_ padding: CGFloat, _ header: UIView) {
-        let stackView = UIStackView(arrangedSubviews: [image2Button, image3Button])
-        stackView.axis = .vertical
-        stackView.distribution = .fillEqually
-        stackView.spacing = padding
-        header.addSubview(stackView)
-        stackView.anchor(top: header.topAnchor, leading: image1Button.trailingAnchor, bottom: header.bottomAnchor, trailing: header.trailingAnchor, padding: .init(top: padding, left: padding, bottom: padding, right: padding))
-    }
-    
-    fileprivate func setupSelectPhotoLargeButton(_ header: UIView, _ padding: CGFloat) {
-        image1Button.anchor(top: header.topAnchor, leading: header.leadingAnchor, bottom: header.bottomAnchor, trailing: nil, padding: .init(top: padding, left: padding, bottom: padding, right: 0))
-        image1Button.widthAnchor.constraint(equalTo: header.widthAnchor, multiplier: 0.45).isActive = true
-    }
-    
-    fileprivate func setupNavigationItems() {
-        navigationItem.title = "Settings"
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
-        navigationItem.rightBarButtonItems = [
-            UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(handleSave)),
-            UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleCancel))
-        ]
-    }
-    
-    @objc fileprivate func handleSelectPhoto(button: UIButton) {
-        let imagePicker = CustomImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.imageButton = button
-        present(imagePicker, animated: true)
-        
-        
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let selectedImage = info[.originalImage] as? UIImage
-        let imageButton = (picker as? CustomImagePickerController)?.imageButton
-        imageButton?.setImage(selectedImage?.withRenderingMode(.alwaysOriginal), for: .normal)
-        dismiss(animated: true, completion: nil)
-    }
-    
-    @objc fileprivate func handleSave() {
-        guard let uid = Auth.auth().currentUser?.uid else {return}
-        
-        //setting user's data that I want to save in Firebase
-        let docData: [String: Any] = [
-            "uid": uid,
-            "fullName": user?.name ?? "",
-            "imageUrl1": user?.imageUrl1 ?? "",
-            "age": user?.age ?? "",
-            "profession": user?.profession ?? ""
-        ]
-        
-        //show HUD as user saving info
-        let hud = JGProgressHUD(style: .dark)
-        hud.textLabel.text = "Saving your profile"
-        hud.show(in: view, animated: true)
-        
-        Firestore.firestore().collection("users").document(uid).setData(docData) { (err) in
-            hud.dismiss(animated: true)
-            if let err = err {
-                print(">>FAILED TO SAVE<<", err)
-                hud.textLabel.text = "Failed to save data"
-                hud.detailTextLabel.text = err.localizedDescription
-                hud.show(in: self.view)
-                hud.dismiss(afterDelay: 5, animated: true)
-                return
-            }
-            
-            print(">>FINISHED SAVIG USER INFO")
-        }
-        
     }
     
     @objc fileprivate func handleNameChange(textField: UITextField) {
@@ -247,9 +233,50 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         self.user?.age = Int(textField.text ?? "")
     }
     
-    @objc fileprivate func handleCancel() {
-        dismiss(animated: true, completion: nil)
+    fileprivate func setupNavigationItems() {
+        navigationItem.title = "Settings"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(handleSave)),
+            UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleCancel))
+        ]
     }
-
+    
+    @objc fileprivate func handleSave() {
+        print("Saving our settings data into Firestore")
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let docData: [String: Any] = [
+            "uid": uid,
+            "fullName": user?.name ?? "",
+            "imageUrl1": user?.imageUrl1 ?? "",
+            "imageUrl2": user?.imageUrl2 ?? "",
+            "imageUrl3": user?.imageUrl3 ?? "",
+            "age": user?.age ?? -1,
+            "profession": user?.profession ?? ""
+        ]
+        
+        let hud = JGProgressHUD(style: .dark)
+        hud.textLabel.text = "Saving settings"
+        hud.show(in: view)
+        Firestore.firestore().collection("users").document(uid).setData(docData) { (err) in
+            hud.dismiss()
+            if let err = err {
+                print(">>>>>Failed to save user settings:", err)
+                hud.textLabel.text = "There was a problem saving your data"
+                hud.detailTextLabel.text = err.localizedDescription
+                hud.show(in: self.view)
+                return
+            }
+            
+            print(">>>>>>>Finished saving user info")
+            print("USER>>>> ", self.user)
+        }
+    }
+    
+    @objc fileprivate func handleCancel() {
+        
+        dismiss(animated: true)
+    }
     
 }

456e7bc85c38935cad277e2363411f84c86ad2db
Eugene Berezin <berezin.nlp@gmail.com>
2019-08-14 16:58:00 -0700

    user detail controller

--- TinderFireStoreEB/TinderFireStoreEB/Controllers/User Details/UserDetailsController.swift
+++ TinderFireStoreEB/TinderFireStoreEB/Controllers/User Details/UserDetailsController.swift
@@ -8,12 +8,70 @@
 
 import UIKit
 
-class UserDetailsController: UIViewController {
+class UserDetailsController: UIViewController, UIScrollViewDelegate {
+    
+    lazy var scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.alwaysBounceVertical = true
+        sv.contentInsetAdjustmentBehavior = .never
+        sv.delegate = self
+        return sv
+        
+    }()
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView(image: #imageLiteral(resourceName: "42490FE4-66B9-488A-9F46-83B5DB38F4AE"))
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    let infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Eugene 30\niOSDev\nPlaceholder bio"
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        view.addSubview(scrollView)
+        scrollView.fillSuperview()
+        
+        scrollView.addSubview(imageView)
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width)
+        
+        scrollView.addSubview(infoLabel)
+        infoLabel.anchor(top: imageView.bottomAnchor, leading: scrollView.leadingAnchor, bottom: nil, trailing: scrollView.trailingAnchor, padding: .init(top: 16, left: 16, bottom: 0, right: 16))
+        
+        
+        dismissUserDetailsController()
+        
 
-       view.backgroundColor = .magenta
+       
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let changeY = -scrollView.contentOffset.y
+        var width = view.frame.width + changeY * 2
+        width = max(view.frame.width, width)
+        imageView.frame = CGRect(x: min(0, -changeY), y: min(0, -changeY), width: width, height: width)
+    }
+    
+    
+    
+    fileprivate func dismissUserDetailsController() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
+    }
+    
+    @objc fileprivate func handleTapDismiss() {
+        self.dismiss(animated: true)
     }
     
 
